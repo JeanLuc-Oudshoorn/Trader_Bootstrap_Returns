962616de2dc3dd0d43c2f8d563e6ba62618b4ad3
JeanLuc-Oudshoorn <jeanluc@oudshoorn.pro>
2022-02-03 21:38:05 +0100

    Wrote a proper script for estimating bootstrapped confidence intervals for long term outperformance versus the S&P500, and visualized these by generating a violinplot.

--- demoanalysis.py
+++ demoanalysis.py
@@ -27,10 +27,10 @@ fig, axs = plt.subplots(3, 4)
 
 axs = axs.ravel()
 
-for i in range(12):
+for i in np.arange(8, 20):
     res = scipy.stats.probplot(popreturn.iloc[:, i].dropna(), dist=scipy.stats.nct,
-    sparams=(fits[i, 0], fits[i, 1], fits[i, 2]), plot=axs[i])
-    axs[i].set_title(str(popreturn.columns[i] + " 's returns QQ-plot"))
+    sparams=(fits[i, 0], fits[i, 1], fits[i, 2]), plot=axs[(i-8)])
+    axs[(i-8)].set_title(str(popreturn.columns[i] + " 's returns QQ-plot"))
 plt.style.use('ggplot')
 plt.show()
 
@@ -40,21 +40,28 @@ b = scipy.stats.nct.rvs(df=fits[0, 0], nc=fits[0, 1], scale=fits[0, 2], size=100
 
 
 # Bootstrap parameter estimation
-means = []
-means1 = []
-l = len(popreturn['jeppe'].dropna())
-l1 = len(popreturn['sp'].dropna())
+traders = ['jeppe', 'christian', 'harry', 'guillaime', 'libor', 'alderique', 'heloise']
+num_replicates = 100000
+stats = np.empty(shape=(num_replicates, len(traders)))
 
-for i in range(100000):
-    sample = np.random.choice(popreturn['jeppe'].dropna().loc[lambda x: x < 0.3], size=l, replace=True)
-    sample1 = np.random.choice(popreturn['sp'].dropna(), size=l1, replace=True)
-    means.append(np.mean(sample))
-    means1.append(np.mean(sample1))
 
-diff = np.array(means) - np.array(means1)
-print(round(np.sum(diff < 0) / len(diff)*100, 2), '%')
+for count, trader in enumerate(traders):
+    means = []
+    l = len(popreturn[trader].loc[lambda x: x < 0.25].dropna())
+    diff = popreturn[trader].loc[lambda x: x < 0.25].dropna() - popreturn['sp'].dropna()[-l::]
 
-sns.set(style="darkgrid")
-fig = sns.kdeplot(diff, shade=True, color="r")
-plt.vlines(0, 0, 60)
+    for i in range(num_replicates):
+        sample = np.random.choice(diff, size=l, replace=True)
+        means.append(np.mean(sample))
+
+    print(trader, 'Percent negative values:', round(np.sum(np.array(means) < 0) / len(means)*100, 2), '%')
+    print(trader, '99% and 95% confidence interval estimates:', np.quantile(means, [0.005, 0.025, 0.5, 0.975, 0.995]))
+
+    stats[:, count] = list(means)
+
+
+stats_df = pd.DataFrame(stats, columns=[traders])
+stats_df = pd.melt(stats_df, var_name='trader')
+
+sns.violinplot(x='trader', y='value', data=stats_df)
 plt.show()
